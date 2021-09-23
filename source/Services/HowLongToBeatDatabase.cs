e24331c15dee3649a96056780c3f9b080e9db1c4
Lacro59 <lacro59@gmail.com>
2021-09-23 15:42:48 +0200

    improved ListWithNoData

--- source/Services/HowLongToBeatDatabase.cs
+++ source/Services/HowLongToBeatDatabase.cs
@@ -364,6 +364,45 @@ namespace HowLongToBeat.Services
             }
         }
 
+        public override void RefreshWithNoData(List<Guid> Ids)
+        {
+            GlobalProgressOptions globalProgressOptions = new GlobalProgressOptions(
+                $"{PluginName} - {resources.GetString("LOCCommonProcessing")}",
+                true
+            );
+            globalProgressOptions.IsIndeterminate = false;
+
+            PlayniteApi.Dialogs.ActivateGlobalProgress((activateGlobalProgress) =>
+            {
+                Stopwatch stopWatch = new Stopwatch();
+                stopWatch.Start();
+
+                activateGlobalProgress.ProgressMaxValue = Ids.Count;
+
+                string CancelText = string.Empty;
+
+                foreach (Guid Id in Ids)
+                {
+                    if (activateGlobalProgress.CancelToken.IsCancellationRequested)
+                    {
+                        CancelText = " canceled";
+                        break;
+                    }
+
+                    Game game = PlayniteApi.Database.Games.Get(Id);
+                    if (game != null)
+                    {
+                        AddData(game);
+                    }
+                    
+                    activateGlobalProgress.CurrentProgressValue++;
+                }
+
+                stopWatch.Stop();
+                TimeSpan ts = stopWatch.Elapsed;
+                logger.Info($"Task Refresh(){CancelText} - {string.Format("{0:00}:{1:00}.{2:00}", ts.Minutes, ts.Seconds, ts.Milliseconds / 10)} for {activateGlobalProgress.CurrentProgressValue}/{Ids.Count} items");
+            }, globalProgressOptions);
+        }
 
         #region Tag
         public override void AddTag(Game game, bool noUpdate = false)
