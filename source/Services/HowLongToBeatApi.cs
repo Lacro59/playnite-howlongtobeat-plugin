3f87c424db9443f52076c635e4c2699bacd50617
Lacro59 <lacro59@gmail.com>
2025-10-16 16:18:02 +0200

    Update HowLongToBeatApi.cs

--- source/Services/HowLongToBeatApi.cs
+++ source/Services/HowLongToBeatApi.cs
@@ -56,8 +56,9 @@ namespace HowLongToBeat.Services
 
         private static string UrlPostData => UrlBase + "/api/submit";
         private static string UrlPostDataEdit => UrlBase + "/submit/edit/{0}";
-        private static string UrlSearchKey => "locate";
-        private static string UrlSearch => UrlBase + "/api/" + UrlSearchKey;
+
+        private static string SearchEndPoint => "/api/locate";
+        private static string UrlSearch => UrlBase + SearchEndPoint;
 
         private static string UrlGameImg => UrlBase + "/games/{0}";
 
@@ -200,7 +201,7 @@ namespace HowLongToBeat.Services
                 {
                     url += $"/_next/static/chunks/pages/{js}";
                     response = await Web.DownloadStringData(url);
-                    Match matches = Regex.Match(response, "\"/api/" + UrlSearchKey + "/\".concat[(]\"(\\w*)\"[)].concat[(]\"(\\w*)\"[)]");
+                    Match matches = Regex.Match(response, $"\"{SearchEndPoint}/\".concat[(]\"(\\w*)\"[)].concat[(]\"(\\w*)\"[)]");
                     SearchId = matches.Groups[1].Value + matches.Groups[2].Value;
                 }
             }
