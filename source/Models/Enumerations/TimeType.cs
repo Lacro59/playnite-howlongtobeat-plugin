c73b29ca9e91d56c0c994b90c1994e54dda33a80
Lacro59 <lacro59@gmail.com>
2026-05-22 15:50:36 +0200

    feat(localization): enhance localization for progress bar and quick search
    Added new localization keys for progress bar time placement and quick search tab. Updated corresponding UI elements to use dynamic resources for improved clarity and user experience.

--- source/Models/Enumerations/TimeType.cs
+++ source/Models/Enumerations/TimeType.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,11 @@ namespace HowLongToBeat.Models.Enumerations
 {
     public enum TimeType
     {
+        [Description("LOCHowLongToBeatMainStory")]
         MainStory,
+        [Description("LOCHowLongToBeatMainExtra")]
         MainStoryExtra,
+        [Description("LOCHowLongToBeatCompletionist")]
         Completionist,
         solo,
         CoOp,
